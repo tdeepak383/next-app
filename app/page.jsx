2b4538c670781a9b9b89c7c0e67ecf847750ac39
tdeepak383 <tdeepak383@gmail.com>
2024-07-20 11:31:49 +0530

    2nd commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import TopicsList from "@/components/TopicsList";
 import Image from "next/image";
+import ShopForSale from "@/components/ShopForSale";
 
 export default function Home() {
   return (
   <>
   <TopicsList />
+  <ShopForSale />
   </>
 );
 }
---
+++ components/ShopForSale.jsx
@@ -0,0 +1,81 @@
+export default function ShopForSale(){
+    return (
+        
+            <section id="shops_for_sale">
+                <div className="container">
+                    <div className="row">
+                    <div className="col-lg-6 col-md-12 col-sm-12">
+                        <div className="shop_for_sale_heading py-4">
+                            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Shops for Sale in Noida / Gr Noida</h2>
+                            <hr />
+                        </div>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Shop for sale in noida under Rs. 30 Lacs</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in shops under Rs. 30 Lacs</p>
+                            </div>
+                        </a>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Shop for sale in noida under Rs. 50 Lacs</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in shops under Rs. 50 Lacs</p>
+                            </div>
+                        </a>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Shop for sale in noida under Rs. 80 Lacs</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in shops under Rs. 80 Lacs</p>
+                            </div>
+                        </a>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Shop for sale in noida under Rs. 1 cr</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in shops under Rs. 1 cr</p>
+                            </div>
+                        </a>
+                        
+                    </div>
+                    <div className="col-lg-6 col-md-12 col-sm-12" >
+                        <div className="shop_for_sale_heading py-4">
+                            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Flats for Sale in Noida / Gr Noida</h2>
+                            <hr />
+                        </div>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">2bhk flats for sale in noida</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in flats under Rs. 30 Lacs</p>
+                            </div>
+                        </a>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">3bhk flats for sale in noida</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in flats under Rs. 50 Lacs</p>
+                            </div>
+                        </a>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">4bhk flats for sale in noida</h5>  
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in flats under Rs. 20 Lacs</p>
+                            </div>
+                        </a>
+                        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-4">
+                            {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt="" /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">2 and 3 bhk flats for sale in noida</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest & Profitable opportunity of investment in flats under Rs. 1 cr</p>
+                            </div>
+                        </a>
+                    </div>  
+                    </div>
+                </div>
+            </section>
+        
+    );
+}
